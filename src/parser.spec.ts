8dd649668c04f94e6919f139daccf9e6b567bb77
raphaelseguin <raphaelseguin@gmail.com>
2025-05-02 18:52:58 +0200

    feat: parser and interpretor first commit

---
+++ src/parser.spec.ts
@@ -0,0 +1,196 @@
+import { describe, expect, test } from "vitest";
+import { Interpretor, Parsing, type Token } from "./parser";
+
+describe("Parsing and interpreting", () => {
+  describe("Parsing", () => {
+    test("Parsing instantiates ", () => {
+      const parsing = new Parsing();
+      expect(() => new Parsing()).not.toThrow();
+    });
+    test("Parse constant A", () => {
+      const parser = new Parsing();
+      const parsed = parser.do([{ type: "constant", value: "A" }]);
+      expect(parsed).toStrictEqual({
+        type: "constant",
+        value: "A",
+      });
+    });
+    test("Parse constant B", () => {
+      const parser = new Parsing();
+      const parsed = parser.do([{ type: "constant", value: "B" }]);
+      expect(parsed).toStrictEqual({
+        type: "constant",
+        value: "B",
+      });
+    });
+    test("Parse constant concat(A, B)", () => {
+      const parser = new Parsing();
+      const parsed = parser.do([
+        { type: "function", value: "concat" },
+        { type: "punctuation", value: "(" },
+        { type: "constant", value: "A" },
+        { type: "punctuation", value: "," },
+        { type: "constant", value: "B" },
+        { type: "punctuation", value: ")" },
+      ]);
+      expect(parsed).toStrictEqual({
+        type: "function",
+        value: "concat",
+        children: [
+          { type: "constant", value: "A" },
+          { type: "constant", value: "B" },
+        ],
+      });
+    });
+
+    test("parse addition of constants", () => {
+      const parser = new Parsing();
+      const parsed = parser.do([
+        { type: "constant", value: "A" },
+        { type: "operation", value: "+" },
+        { type: "constant", value: "B" },
+      ]);
+      expect(parsed).toStrictEqual({
+        type: "operation",
+        value: "+",
+        children: [
+          { type: "constant", value: "A" },
+          { type: "constant", value: "B" },
+        ],
+      });
+    });
+
+    test("parse addition of three constants", () => {
+      const parser = new Parsing();
+      const parsed = parser.do([
+        { type: "constant", value: "A" },
+        { type: "operation", value: "+" },
+        { type: "constant", value: "B" },
+        { type: "operation", value: "+" },
+        { type: "constant", value: "C" },
+      ]);
+      expect(parsed).toStrictEqual({
+        type: "operation",
+        value: "+",
+        children: [
+          { type: "constant", value: "A" },
+          {
+            type: "operation",
+            value: "+",
+            children: [
+              { type: "constant", value: "B" },
+              { type: "constant", value: "C" },
+            ],
+          },
+        ],
+      });
+    });
+  });
+  describe("Interpreting", () => {
+    test("constant yields same value", () => {
+      const result = new Interpretor({}).execute({
+        type: "constant",
+        value: "my value",
+        children: [],
+      });
+      expect(result).toStrictEqual("my value");
+    });
+    test("addition of two constants", () => {
+      const result = new Interpretor({}).execute({
+        type: "operation",
+        value: "+",
+        children: [
+          { type: "constant", value: 12, children: [] },
+          { type: "constant", value: "34", children: [] },
+        ],
+      });
+      expect(result).toStrictEqual(46);
+    });
+    test("addition of two variables", () => {
+      const result = new Interpretor({ a: 123, b: 321 }).execute({
+        type: "operation",
+        value: "+",
+        children: [
+          { type: "variable", value: "a", children: [] },
+          { type: "variable", value: "b", children: [] },
+        ],
+      });
+      expect(result).toStrictEqual(444);
+    });
+    test("function concat on variables", () => {
+      const result = new Interpretor({ a: "hello", b: " world" }).execute({
+        type: "function",
+        value: "concat",
+        children: [
+          { type: "variable", value: "a", children: [] },
+          { type: "variable", value: "b", children: [] },
+        ],
+      });
+      expect(result).toStrictEqual("hello world");
+    });
+    test("function concat on variables", () => {
+      const result = new Interpretor({ a: "hello", b: " world" }).execute({
+        type: "function",
+        value: "concat",
+        children: [
+          { type: "variable", value: "a", children: [] },
+          { type: "variable", value: "b", children: [] },
+        ],
+      });
+      expect(result).toStrictEqual("hello world");
+    });
+    test("concat operations (nesting)", () => {
+      const result = new Interpretor({ a: 12, b: 34 }).execute({
+        type: "function",
+        value: "concat",
+        children: [
+          {
+            type: "constant",
+            value: "L'âge du capitaine est : ",
+            children: [],
+          },
+          {
+            type: "operation",
+            value: "+",
+            children: [
+              {
+                type: "variable",
+                value: "a",
+                children: [],
+              },
+              {
+                type: "variable",
+                value: "b",
+                children: [],
+              },
+            ],
+          },
+        ],
+      });
+      expect(result).toStrictEqual("L'âge du capitaine est : 46");
+    });
+  })
+  describe('End to end', () => {
+    test('Concat variable and replace', () => {
+      const formula: Token[] = [
+          { type: "function", value: "replace" },
+          { type: "punctuation", value: "(" },
+          { type: "function", value: "concat" },
+          { type: "punctuation", value: "(" },
+          { type: "variable", value: "a" },
+          { type: "punctuation", value: "," },
+          { type: "variable", value: "b" },
+          { type: "punctuation", value: ")" },
+          { type: "punctuation", value: "," },
+          { type: "constant", value: "Morceau" },
+          { type: "punctuation", value: "," },
+          { type: "constant", value: "Bout" },
+          { type: "punctuation", value: ")" },
+        ]
+      const parsed = new Parsing().do(formula)
+      const context = {a: "Marabout, ", b: "Morceau de ficelle"}
+      const result = new Interpretor(context).execute(parsed)
+      expect(result).toStrictEqual("Marabout, Bout de ficelle")
+    })
+  })
+});
---
+++ src/parser.ts
@@ -0,0 +1,195 @@
+export type Token = Function | Constant | Punctuation | Operation | Variable;
+type Function = {
+  type: "function";
+  value: "add" | "concat" | "replace";
+};
+type Constant = {
+  type: "constant";
+  value: number | string;
+};
+type Punctuation = {
+  type: "punctuation";
+  value: "," | "(" | ")";
+};
+type Operation = {
+  type: "operation";
+  value: "+" | "-" | "*" | "/";
+};
+type Variable = {
+  type: "variable";
+  value: string;
+};
+type ParseTree = Token & {
+  children?: ParseTree[];
+};
+
+export class Parsing {
+  constructor() {}
+  do(tokens: Token[]): ParseTree {
+    return this.parse(tokens)[0];
+  }
+  private parse(tokens: ParseTree[]): ParseTree[] {
+    if (tokens.every((token) => this.isLeaf(token))) {
+      return tokens;
+    }
+    if (this.isOperation(tokens)) {
+      return [this.parseOperation(tokens)];
+    }
+    if (this.isFunction(tokens)) {
+      return [this.parseFunction(tokens)];
+    }
+    throw new Error("not goood");
+  }
+  private isLeaf(token: Token): boolean {
+    return ["constant", "variable"].includes(token.type);
+  }
+  private parseOperation(tokens: ParseTree[]): ParseTree {
+    const operationIndex = tokens.findIndex(
+      (token) => token.type === "operation"
+    );
+    const leftOperand = tokens.slice(0, operationIndex);
+    const operation = tokens[operationIndex];
+    const rightOperand = tokens.slice(operationIndex + 1, tokens.length);
+    operation.children = [
+      ...this.parse(leftOperand),
+      ...this.parse(rightOperand),
+    ];
+    return operation;
+  }
+  private parseFunction(tokens: ParseTree[]): ParseTree {
+    const value = tokens[0].value as Function["value"];
+    const functionArguments = this.removeOutterParens(tokens.slice(1));
+
+    return {
+      type: "function",
+      value,
+      children: this.splitArguments(functionArguments).map(
+        (tokens) => this.parse(tokens)[0]
+      ),
+    };
+  }
+  private splitArguments(tokens: Token[]): Token[][] {
+    let parenLevel = 0;
+    const splittedTokens = [[]] as Token[][];
+    for (const token of tokens) {
+      if (token.type === "punctuation" && ["(", ")"].includes(token.value)) {
+        parenLevel += token.value === "(" ? 1 : -1;
+      }
+      if (parenLevel === 0 && token.value === ",") {
+        splittedTokens.push([]);
+      } else {
+        splittedTokens.at(-1)?.push(token);
+      }
+    }
+    return splittedTokens;
+  }
+  private removeOutterParens(tokens: Token[]): Token[] {
+    const openParenIndex = tokens.findIndex(({ value }) => value === "(");
+    const closeParenIndex = tokens.reduce(
+      (lastIndex, token, index) => (token.value === ")" ? index : lastIndex),
+      0
+    );
+    return tokens.filter(
+      (_, i) => i !== openParenIndex && i !== closeParenIndex
+    );
+  }
+  private isOperation(tokens: Token[]): boolean {
+    let parenLevel = 0;
+    for (const { value, type } of tokens) {
+      if (type === "punctuation") {
+        parenLevel += value === "(" ? 1 : -1;
+      }
+      if (parenLevel === 0 && type === "operation") {
+        return true;
+      }
+    }
+    return false;
+  }
+  private isFunction(tokens: Token[]): boolean {
+    if (this.isOperation(tokens)) {
+      return false;
+    }
+    return tokens.some(({ type }) => type === "function");
+  }
+}
+
+export class Interpretor {
+  constructor(private readonly context: Record<string, number | string>) {}
+  execute(parseTree: ParseTree): number | string {
+    return parseTree.type === "operation"
+      ? this.interpretOperation(parseTree)
+      : parseTree.type === "function"
+      ? this.interpretFunction(parseTree)
+      : parseTree.type === "constant"
+      ? this.interpretConstant(parseTree)
+      : parseTree.type === "variable"
+      ? this.interpretVariable(parseTree)
+      : "ERROR";
+  }
+  private interpretConstant({
+    value,
+  }: Constant & { children?: ParseTree[] }): string | number {
+    return value;
+  }
+  private interpretVariable({
+    value,
+  }: Variable & { children?: ParseTree[] }): string | number {
+    if (!Object.keys(this.context).includes(value)) {
+      throw new Error(`Variable ${value} undefined in context`);
+    }
+    return this.context[value];
+  }
+  private interpretFunction({
+    value,
+    children = [],
+  }: Function & { children?: ParseTree[] }): string | number {
+    return value === "concat"
+      ? this.concat(...children)
+      : value === "replace"
+      ? this.replace(...children)
+      : "ERROR";
+  }
+  private interpretOperation({
+    value,
+    children = [],
+  }: Operation & { children?: ParseTree[] }): string | number {
+    const [leftOperand, rightOperand] = children;
+    return value === "+"
+      ? this.add(leftOperand, rightOperand)
+      : value === "-"
+      ? this.subtract(leftOperand, rightOperand)
+      : value === "*"
+      ? this.multiply(leftOperand, rightOperand)
+      : this.divide(leftOperand, rightOperand);
+  }
+  private replace(...args: ParseTree[]): string {
+    const [source, target, replacement] = args;
+    return `${this.execute(source)}`.replaceAll(
+      `${this.execute(target)}`,
+      `${this.execute(replacement)}`
+    );
+  }
+  private concat(...args: ParseTree[]): string {
+    return args.map((node) => this.execute(node)).join("");
+  }
+  private divide(leftOperand: ParseTree, rightOperand: ParseTree): number {
+    return (
+      Number(this.execute(leftOperand)) / Number(this.execute(rightOperand))
+    );
+  }
+  private multiply(leftOperand: ParseTree, rightOperand: ParseTree): number {
+    return (
+      Number(this.execute(leftOperand)) * Number(this.execute(rightOperand))
+    );
+  }
+  private subtract(leftOperand: ParseTree, rightOperand: ParseTree): number {
+    return (
+      Number(this.execute(leftOperand)) - Number(this.execute(rightOperand))
+    );
+  }
+  private add(leftOperand: ParseTree, rightOperand: ParseTree): number {
+    return (
+      Number(this.execute(leftOperand)) + Number(this.execute(rightOperand))
+    );
+  }
+}
